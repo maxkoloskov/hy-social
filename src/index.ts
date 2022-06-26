44276bc0f1a38970a10b4ab7cbe6ee1c06544414
Max Koloskov <koloskov.maxim@mail.ru>
2022-06-26 21:12:42 +0300

    :tada: Initial

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use('*', (_, res) => {
+  res.status(404).json({ message: 'Not Found' });
+})
+
+app.listen(1337, () => {
+  console.log('App started');
+});
